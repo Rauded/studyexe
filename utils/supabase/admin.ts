c5b4755af40f6242d008bce8465c6c56838b3a3a
Eduard Hvižďak <145847649+Rauded@users.noreply.github.com>
2026-01-11 14:56:58 +0100

    subscription fix

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -234,8 +234,38 @@ const manageSubscriptionStatusChange = async (
   });
   // Upsert the latest status of the subscription object.
   const subscriptionData = {
+    id: subscription.id,
     user_id: uuid,
+    metadata: subscription.metadata,
     status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+    //TODO check quantity on subscription
+    // @ts-ignore
+    quantity: subscription.quantity,
+    cancel_at_period_end: subscription.cancel_at_period_end,
+    cancel_at: subscription.cancel_at
+      ? toDateTime(subscription.cancel_at).toISOString()
+      : null,
+    canceled_at: subscription.canceled_at
+      ? toDateTime(subscription.canceled_at).toISOString()
+      : null,
+    current_period_start: toDateTime(
+      subscription.current_period_start
+    ).toISOString(),
+    current_period_end: toDateTime(
+      subscription.current_period_end
+    ).toISOString(),
+    created: toDateTime(subscription.created).toISOString(),
+    ended_at: subscription.ended_at
+      ? toDateTime(subscription.ended_at).toISOString()
+      : null,
+    trial_start: subscription.trial_start
+      ? toDateTime(subscription.trial_start).toISOString()
+      : null,
+    trial_end: subscription.trial_end
+      ? toDateTime(subscription.trial_end).toISOString()
+      : null,
+    // Keep the simplified fields for convenience
     plan: subscription.items.data[0].price.nickname || subscription.items.data[0].price.id,
     expires_at: toDateTime(subscription.current_period_end).toISOString()
   };
