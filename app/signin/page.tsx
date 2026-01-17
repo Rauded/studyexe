2efb653108d4d0059825c2e0f98dbc7ec24e2a0e
Eduard Hvižďak <145847649+Rauded@users.noreply.github.com>
2026-01-17 20:12:18 +0100

    fix

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,10 +2,28 @@ import { redirect } from 'next/navigation';
 import { getDefaultSignInView } from '@/utils/auth-helpers/settings';
 import { cookies } from 'next/headers';
 
-export default function SignIn() {
+export default function SignIn({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const preferredSignInView =
     cookies().get('preferredSignInView')?.value || null;
   const defaultView = getDefaultSignInView(preferredSignInView);
 
-  return redirect(`/signin/${defaultView}`);
+  let queryString = '';
+  if (searchParams) {
+    const params = new URLSearchParams();
+    Object.entries(searchParams).forEach(([key, value]) => {
+      if (typeof value === 'string') {
+        params.append(key, value);
+      } else if (Array.isArray(value)) {
+        value.forEach((v) => params.append(key, v));
+      }
+    });
+    const str = params.toString();
+    if (str) queryString = `?${str}`;
+  }
+
+  return redirect(`/signin/${defaultView}${queryString}`);
 }
