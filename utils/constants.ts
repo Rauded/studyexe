e81fe56dd246456a9a4d31bc3c95b44b3fff77f6
Eduard Hvižďak <145847649+Rauded@users.noreply.github.com>
2026-01-17 17:44:57 +0100

    download lingk

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,1 @@
-export const DOWNLOAD_LINK = "https://pub-ab1914aff12b4b61b8a0760d08df8c48.r2.dev/StudyExe_v0.1.3.zip";
+export const DOWNLOAD_LINK = "https://pub-ab12439ff097157607bee00a98a63c2e14.r2.dev/StudyExe_v0.1.4.zip";
